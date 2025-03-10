86fc702c44e8dd037fcfb5259d74ce4194dc0287
Apthoo <apuroykdi@gmail.com>
2025-03-11 03:19:40 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Make welcome.sh executable
-chmod +x welcome.sh
+sudo chmod +x welcome.sh
 
 # Add welcome.sh to .bash_profile or .bashrc
 if [ -f ~/.bash_profile ]; then
@@ -25,4 +25,22 @@ echo "                         +=================+
                          +=================+                                                          
                   "
 
-                      "
+
+# Prompt the user for input
+read -p "Do you want to reboot the system? (yes/y or no/n): " user
+
+# Convert input to lowercase
+user=$(echo "$user" | tr '[:upper:]' '[:lower:]')
+
+# Check the user's input
+if [[ "$user" == "yes" || "$user" == "y" ]]; then
+    echo "Rebooting the system..."
+    sudo reboot
+elif [[ "$user" == "no" || "$user" == "n" ]]; then
+    echo "No action taken. Exiting..."
+    exit
+else
+    echo "Invalid input. Please enter 'yes/y' to reboot or 'no/n' to exit."
+    exit 1
+fi
+                      
