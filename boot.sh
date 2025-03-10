8c60416c22ba27f5c797d54938b00c19abbb5b75
Apthoo <apuroykdi@gmail.com>
2025-03-11 02:54:13 +0530

    Create boot.sh

---
+++ boot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Make scripts executable
+chmod u+x welcome.sh
+chmod u+x setup.sh
+
+# Run the setup 1
+./welcome.sh
+
+# Run the setup script
+./setup.sh
+
+echo "Setup complete! Please restart your terminal to see the changes."
