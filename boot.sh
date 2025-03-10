9888b5f79b2f1c685fcb28f9a671af84acd11309
Apthoo <apuroykdi@gmail.com>
2025-03-11 03:20:53 +0530

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Make scripts executable
-chmod u+x welcome.sh
-chmod u+x setup.sh
+sudo chmod u+x welcome.sh
+sudo chmod u+x setup.sh
 
 # Run the setup 1
 ./welcome.sh
@@ -11,3 +11,22 @@ chmod u+x setup.sh
 ./setup.sh
 
 echo "Setup complete! Please restart your terminal to see the changes."
+
+
+# Prompt the user for input
+read -p "Do you want to reboot the system? (yes/y or no/n): " user
+
+# Convert input to lowercase
+user=$(echo "$user" | tr '[:upper:]' '[:lower:]')
+
+# Check the user's input
+if [[ "$user" == "yes" || "$user" == "y" ]]; then
+    echo "Rebooting the system..."
+    sudo reboot
+elif [[ "$user" == "no" || "$user" == "n" ]]; then
+    echo "No action taken. Exiting..."
+    exit
+else
+    echo "Invalid input. Please enter 'yes/y' to reboot or 'no/n' to exit."
+    exit 1
+fi
